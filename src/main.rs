5170a24fe4d3f076b4cecac79fcc52ebbd61b096
egas7t <egas11310@gmail.com>
2023-09-13 09:44:17 +0100

    e

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 
 static HELLO: &[u8] = b"epic gamer!";
